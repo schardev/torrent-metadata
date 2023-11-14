23699cb8c83142860000741bf352e22204ae2c7c
Saurabh Charde <saurabhchardereal@gmail.com>
2023-11-14 21:35:41 +0530

    separate file and hash query
    - for better error responses

--- server/src/middlewares.ts
+++ server/src/middlewares.ts
@@ -1,26 +1,44 @@
 import { ErrorRequestHandler, RequestHandler } from "express";
-import { error, log } from "./utils.js";
 import parseTorrent from "parse-torrent";
+import { error, log } from "./utils.js";
 
 export const isValidTorrentData: RequestHandler = async (req, res, next) => {
   log("Validating torrent query...");
 
-  const query = (req.body.query as string) || req.file?.buffer;
-  if (!query) {
+  const hashOrURIQuery = req.body.query as string;
+  const torrentFileQuery = req.file;
+
+  if (!hashOrURIQuery && !torrentFileQuery) {
     return res.status(400).json({
       error:
         "Expected info hash, magnet URI or .torrent file, instead got nothing.",
     });
   }
 
-  try {
-    req.parsedTorrent = await parseTorrent(query);
-  } catch (err) {
-    log(err);
-    return res.status(400).json({
-      error: "Invalid info hash, magnet URI or .torrent file.",
-      query: typeof query === "string" ? query : req.file?.originalname || "",
-    });
+  if (hashOrURIQuery) {
+    try {
+      log("Parsing hash or URI...");
+      req.parsedTorrent = await parseTorrent(hashOrURIQuery);
+    } catch (err) {
+      log(err);
+      return res.status(400).json({
+        error: "Invalid info hash or magnet URI.",
+        query: hashOrURIQuery,
+      });
+    }
+  }
+
+  if (torrentFileQuery) {
+    try {
+      log("Parsing torrent file...");
+      req.parsedTorrent = await parseTorrent(torrentFileQuery.buffer);
+    } catch (err) {
+      log(err);
+      return res.status(400).json({
+        error: "Torrent file is invalid.",
+        query: torrentFileQuery.originalname,
+      });
+    }
   }
 
   log("Validated torrent query.");
