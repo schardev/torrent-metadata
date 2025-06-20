3c27cb0e3f1506f48d7345a4588ea839ab660e72
Saurabh Charde <saurabhchardereal@gmail.com>
2025-06-20 15:55:31 +0530

    feat: move from next js to vite

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -2,16 +2,10 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-family-sans)", ...defaultTheme.fontFamily.sans],
-      },
+      fontFamily: { sans: ["Inter", ...defaultTheme.fontFamily.sans] },
     },
   },
   plugins: [],
